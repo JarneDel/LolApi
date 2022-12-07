c5ce5c6bc94ea6021ce8ac4f51847511dc90ff9f
jarneDel <jarne.delarue@student.howest.be>
2022-12-07 08:37:52 +0100

    updated packages

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const getMatchRouter = require("./routes/api/getMatchPerPlayer");
 const getRouter = require("./routes/api/getRoutes");
@@ -15,13 +13,12 @@ const app = express();
 // dev use only
 // todo: remove this
 app.use(cors());
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/", getRouter);
 app.use("/api/v2/", getMatchRouter);
 app.use("/", indexRouter);
+
 
 module.exports = app;
