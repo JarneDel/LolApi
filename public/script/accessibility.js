8046e37391deb0f6b09fa079fa8ddc9f369231a0
jarneDel <jarne.delarue@student.howest.be>
2022-12-19 21:51:43 +0100

    Code Cleanup!

--- public/script/accessibility.js
+++ public/script/accessibility.js
@@ -1,4 +1,4 @@
-function a11yClick(event){
+export function a11yClick(event){
   console.log(event.type, event.charCode, event.keyCode);
   if(event.type === 'click'){
     return true;
@@ -12,4 +12,31 @@ function a11yClick(event){
   else{
     return false;
   }
+}
+
+export const trapFocus = function (element) {
+  let focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+  let firstFocusableEl = focusableEls[0];
+  let lastFocusableEl = focusableEls[focusableEls.length - 1];
+  let KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function (e) {
+    let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) /* shift + tab */ {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+        e.preventDefault();
+      }
+    } else /* tab */ {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+        e.preventDefault();
+      }
+    }
+  });
 }
---
+++ public/script/filter.js
@@ -0,0 +1,63 @@
+import {a11yClick} from './accessibility.js';
+
+const tagClicked = (e, item, ListOfFilters) => {
+    ListOfFilters.forEach((filter) => {
+        filter.classList.remove('u-selected');
+    });
+    item.classList.add('u-selected');
+    // show the champions that have the tag
+    document.querySelectorAll('.js-card-box').forEach((cardContainer) => {
+        let card = cardContainer.firstElementChild;
+        let tags = card.dataset.tags.split(',');
+        // console.log(tags);
+        if (tags.includes(item.dataset.tag) || item.dataset.tag === 'All') {
+            cardContainer.classList.remove('u-hidden-3');
+        } else {
+            cardContainer.classList.add('u-hidden-3');
+        }
+    });
+};
+
+export function listenToTags(){
+    const ListOfFilters = document.querySelectorAll('.js-filter-item');
+    ListOfFilters.forEach((item) => {
+        // two events for accessibility
+        item.addEventListener("click", (e) => {
+            tagClicked(e, item, ListOfFilters);
+        });
+        // so that the element is selectable with the keyboard
+        item.addEventListener('keyup', (e) => {
+            if (a11yClick(e)) {
+                // console.log("a11y click");
+                tagClicked(e, item, ListOfFilters);
+            }
+        });
+    });
+}
+
+
+export const listenToSearchChampion = () => {
+    const searchChampion = document.querySelector('.js-search-champion');
+    if (searchChampion.value !== null){
+        sortByChampion(searchChampion.value);
+    }
+    searchChampion.addEventListener('input', () => {
+        const value = searchChampion.value.toLowerCase();
+        sortByChampion(value);
+    });
+};
+
+
+const sortByChampion = (query) =>{
+    query = query.trim().toLowerCase();
+    const cards = document.querySelectorAll('.js-card-box');
+    cards.forEach((card) => {
+        const name = card.querySelector('.c-card__title').innerText.toLowerCase();
+        if (name.includes(query)) {
+            card.classList.remove('u-hidden-2');
+
+        } else {
+            card.classList.add('u-hidden-2');
+        }
+    });
+}
